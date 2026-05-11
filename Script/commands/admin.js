088ab0a9f813af68507ba836756c642876644ec9
newpppp22 <new0gmai@gmail.com>
2026-05-11 22:11:15 +0600

    Update and rename 0admin.js to admin.js

--- Script/commands/admin.js
+++ Script/commands/admin.js
@@ -2,10 +2,10 @@ const{ writeFileSync, existsSync } = require("fs-extra");
 const { resolve } = require("path");
 
 module.exports.config = {
-  name: "0admin",
+  name: "admin",
   version: "2.0.0", //don't change credit
   hasPermssion: 2,
-  credits: "Parves Sheikh,
+  credits: "Parves Sheikh",
   description: "Admin Management System",
   commandCategory: "Admin",
   usages: "[list | add | remove | only | boxonly] [uid | @mention | reply]",
