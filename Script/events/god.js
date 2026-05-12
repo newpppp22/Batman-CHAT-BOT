934624ea7f03738eaae6aba800465dca63b9f2c0
newpppp22 <new0gmai@gmail.com>
2026-05-12 22:14:44 +0600

    Update god.js

--- Script/events/god.js
+++ Script/events/god.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "god",
 	eventType: ["log:unsubscribe", "log:subscribe", "log:thread-name"],
 	version: "1.0.0",
-	credits: "SHAHADAT SAHU",
+	credits: "Parves Wayne",
 	description: "Record bot activity notifications!",
 	envConfig: {
 		enable: true
@@ -13,7 +13,7 @@ module.exports.run = async function({ api, event, Threads }) {
 	const logger = require("../../utils/log");
 	if (!global.configModule[this.config.name].enable) return;
 	
-	let formReport = "=== ─꯭─⃝‌‌𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭 Notification ===" +
+	let formReport = "=== 𑣲⋆ 𝐘𝐮𝐦𝐞𝐤𝐨 𝐁𝐛𝐳 𐙚 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭 Notification ===" +
 					"\n\n» Thread ID: " + event.threadID +
 					"\n» Action: {task}" +
 					"\n» Action created by userID: " + event.author +
@@ -50,7 +50,7 @@ module.exports.run = async function({ api, event, Threads }) {
 	formReport = formReport.replace(/\{task}/g, task);
 
 	const receivers = [
-		"100001039692046",   // Replace youR UID
+		"100080580662648",   // Replace youR UID
 		"2056569868083458"   //  Replace youR Group UID
 	];
 
