f44136b25f2dfd0e23fc41eb148e446c29038524
newpppp22 <new0gmai@gmail.com>
2026-05-14 11:29:22 +0600

    Update rules.js

--- Script/commands/rules.js
+++ Script/commands/rules.js
@@ -2,7 +2,7 @@ module.exports.config = {
  name: "rules",
  version: "1.0.0",
  hasPermssion: 0,
- credits: "SHAHADAT SAHU",
+ credits: "Parves Wayne",
  description: "Send group rules",
  commandCategory: "information",
  usages: "rules2",
@@ -29,10 +29,10 @@ module.exports.run = async ({ api, event }) => {
 ❖ কারও সমস্যা থাকলে সরাসরি 𝙰𝙳𝙼𝙸𝙽𝚂 দের ইনবক্স করুন 💌
 ❖ রুলস ভাঙলে আগে ওয়ার্নিং, পরে অ্যাকশন 😈
 
-𝙱𝙾𝚃 𝙰𝙳𝙼𝙸𝙽: SHAHADAT SAHU
-𝙵𝙱 𝙻𝙸𝙽𝙺: https://www.facebook.com/100044713412032
+𝙱𝙾𝚃 𝙰𝙳𝙼𝙸𝙽: 愛 𝐏𝐚𝐫𝐯𝐞𝐬 𝐖𝐚𝐲𝐧𝐞 𒌐
+𝙵𝙱 𝙻𝙸𝙽𝙺: https://www.facebook.com/100080580662648
 
-_সাথেই থাকুন 🌺─꯭─⃝‌‌𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭🌸_
+_সাথেই থাকুন 𑣲⋆ 𝐘𝐮𝐦𝐞𝐤𝐨 𝐁𝐛𝐳 𐙚 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭
 
 💖...........ধন্যবাদ সবাইকে...........💖`;
 
