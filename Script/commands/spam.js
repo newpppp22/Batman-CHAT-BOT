b34b9849da91c387584dde147d01a9db65f5eefd
newpppp22 <new0gmai@gmail.com>
2026-05-14 08:57:39 +0600

    Update spam.js

--- Script/commands/spam.js
+++ Script/commands/spam.js
@@ -2,20 +2,28 @@ module.exports.config = {
   name: "spam",
   version: "1.0.0",
   hasPermssion: 2,
-  credits: "Parves Wayne",
+  credits: "SHAHADAT SAHU",
   description: "Spam messages",
   commandCategory: "spam",
-  usages: "[msg] [amount]",
+  usages: "[message] [amount]",
   cooldowns: 5
 };
 
 module.exports.run = async function ({ api, event, args }) {
 
+  if (args.length < 2) {
+    return api.sendMessage(
+      "Usage: spam [message] [amount]",
+      event.threadID,
+      event.messageID
+    );
+  }
+
   const count = parseInt(args[args.length - 1]);
 
   if (isNaN(count)) {
     return api.sendMessage(
-      "Usage: spam [message] [amount]",
+      "Last value must be a number!",
       event.threadID,
       event.messageID
     );
